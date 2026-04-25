0e8f36fd9907eadb76e9320583c360595e4cc356
kaele-rise <xodorov06@gmail.com>
2026-04-25 13:23:00 +1000

    upd.

---
+++ src/lab12/view.php
@@ -0,0 +1,228 @@
+<html>
+	<head>
+		<title>ЭОС ВВГУ</title>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--===============================================================================================-->
+		<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+		<!--===============================================================================================-->
+		<link
+			rel="stylesheet"
+			type="text/css"
+			href="vendor/bootstrap/css/bootstrap.min.css"
+		/>
+		<!--===============================================================================================-->
+		<link
+			rel="stylesheet"
+			type="text/css"
+			href="fonts/font-awesome-4.7.0/css/font-awesome.min.css"
+		/>
+		<!--===============================================================================================-->
+		<link
+			rel="stylesheet"
+			type="text/css"
+			href="fonts/iconic/css/material-design-iconic-font.min.css"
+		/>
+		<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css" />
+		<!--===============================================================================================-->
+		<link
+			rel="stylesheet"
+			type="text/css"
+			href="vendor/css-hamburgers/hamburgers.min.css"
+		/>
+		<!--===============================================================================================-->
+		<link
+			rel="stylesheet"
+			type="text/css"
+			href="vendor/animsition/css/animsition.min.css"
+		/>
+		<!--===============================================================================================-->
+		<link
+			rel="stylesheet"
+			type="text/css"
+			href="vendor/select2/select2.min.css"
+		/>
+		<!--===============================================================================================-->
+		<link
+			rel="stylesheet"
+			type="text/css"
+			href="vendor/daterangepicker/daterangepicker.css"
+		/>
+		<!--===============================================================================================-->
+		<link rel="stylesheet" type="text/css" href="css/util.css" />
+		<link rel="stylesheet" type="text/css" href="css/main.css" />
+		<!--===============================================================================================-->
+	</head>
+	<body>
+		<div class="limiter">
+			<div
+				style="
+					background-image: url('images/bg-07.jpg');
+					background-position: 50% 90%;"
+			>
+				<div class="container-login100"
+					style="
+						background-image: linear-gradient(
+							25deg,
+							rgba(228, 228, 228, 0.65) 0%,
+							rgba(252, 252, 252, 0.7) 60%,
+							rgba(228, 228, 228, 0.65) 100%);
+						backdrop-filter: blur(15px);"
+				>
+				<div id="root" class="wrap-login100 p-l-55 p-r-55 p-t-65 p-b-54">
+					<form
+						class="login-form login100-form validate-form"
+						action="https://eos.vvsu.ru/login/index.php"
+						method="POST"
+					>
+						<span class="login100-form-title p-b-49"
+							><a href="https://www.vvsu.ru/" target="_blank"
+								><img src="images/logo_b.svg" width="170"
+							/></a>
+							<p class="p-t-18" style="font-size: 18px">
+								Электронная образовательная&nbsp;среда
+							</p>
+						</span>
+
+						<div
+							class="wrap-input100 validate-input m-b-23"
+							data-validate="Требуется логин"
+						>
+							<span class="label-input100">Логин</span>
+							<input
+								class="input100"
+								type="text"
+								name="username"
+								placeholder="Введите логин"
+								value=""
+							/>
+							<span class="focus-input100" data-symbol="&#xf206;"></span>
+						</div>
+
+						<div
+							class="wrap-input100 validate-input"
+							data-validate="Требуется пароль"
+						>
+							<span class="label-input100">Пароль</span>
+							<input
+								class="input100"
+								type="password"
+								name="password"
+								placeholder="Введите пароль"
+							/>
+							<span class="focus-input100" data-symbol="&#xf190;"></span>
+						</div>
+						<div
+							class="login_error text-right p-t-8 p-b-8"
+							id="err"
+							style="color: #d9414d; font-size: 14px"
+						></div>
+
+						<div class="text-right p-t-15 p-b-45">
+							<p class="p-t-5">
+								<a href="https://api.vvsu.ru/services/changepass">
+									Я забыл(а) пароль, и я студент/сотрудник ВВГУ
+								</a>
+							</p>
+							<p class="p-t-5">
+								<a href="https://apply.vvsu.ru/resetPass/">
+									Я забыл(а) пароль, и я внешний пользователь
+								</a>
+							</p>
+						</div>
+
+						<div class="container-login100-form-btn">
+							<div class="wrap-login100-form-btn">
+								<div class="login100-form-bgbtn"></div>
+								<button class="login100-form-btn" name="btnlogin" type="submit">
+									Войти
+								</button>
+							</div>
+						</div>
+
+						<!-- <div class="txt1 text-center p-t-45 p-b-20">
+							<span> Или войти с использованием учетной записи </span>
+						</div> -->
+
+						<div class="container-login100-form-btn p-t-35">
+							<div class="wrap-login100-form-btn">
+								<div class="login100-form-bgbtn"></div>
+								<button class="login100-form-btn">
+									<a
+										href="https://eos.vvsu.ru/auth/oidc"
+										class="login100-form-btn"
+									>
+										<img
+											src="images/icons/oe_favicon.ico"
+											width="24"
+											height="24"
+											style="margin-right: 0.4em"
+										/>
+										Моё образование</a
+									>
+								</button>
+							</div>
+						</div>
+					</form>
+				</div></div>
+			</div>
+		</div>
+		<div class="text-center p-t-85 p-b-20 footer" style="background: #292929">
+			<h3 class="p-b-20 p-l-55 p-r-55">Поддержка</h3>
+			<h5 class="p-t-8 p-b-20 p-l-55 p-r-55">Центр цифровых решений</h5>
+			<p class="p-t-8 p-l-55 p-r-55">
+				E-mail:
+				<a href="mailto:2404046@vvsu.ru">2404046@vvsu.ru</a>
+				Тел.:&nbsp;+7&nbsp;(423)&nbsp;240-40-46, +7&nbsp;(423)&nbsp;240-42-38
+			</p>
+			<p class="p-b-55 p-l-55 p-r-55">
+				г.&nbsp;Владивосток, ул.&nbsp;Гоголя,&nbsp;41, ауд.&nbsp;1326
+			</p>
+			<hr />
+			<h5 class="p-t-55 p-b-20 p-l-55 p-r-55">Системные требования</h5>
+			<p class="p-b-55 p-l-55 p-r-55">
+				<a href="sysreq.pdf" target="_blank"
+					>Скачать минимальные технические требования к аппаратному и
+					программному обеспечению, каналу связи пользователя</a
+				>
+			</p>
+			<hr />
+			<p class="p-t-8 p-b-8 p-l-55 p-r-55">
+				© ВВГУ. Все права защищены.
+				<script>
+					var currentTime = new Date()
+					document.write(
+						' 2013&nbsp;-&nbsp;' + currentTime.getFullYear() + '. '
+					)
+				</script>
+				Техническая поддержка и развитие системы:
+				<a
+					href="https://www.vvsu.ru/about/divisions/academic/dep/id/1150018/"
+					target="_blank"
+					>ЦЦР ВВГУ
+				</a>
+			</p>
+		</div>
+
+		<div id="dropDownSelect1"></div>
+
+		<!--===============================================================================================-->
+		<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+		<!--===============================================================================================-->
+		<script src="vendor/animsition/js/animsition.min.js"></script>
+		<!--===============================================================================================-->
+		<script src="vendor/bootstrap/js/popper.js"></script>
+		<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+		<!--===============================================================================================-->
+		<script src="vendor/select2/select2.min.js"></script>
+		<!--===============================================================================================-->
+		<script src="vendor/daterangepicker/moment.min.js"></script>
+		<script src="vendor/daterangepicker/daterangepicker.js"></script>
+		<!--===============================================================================================-->
+		<script src="vendor/countdowntime/countdowntime.js"></script>
+		<!--===============================================================================================-->
+		<script src="js/main.js"></script>		
+
+	</body>
+</html>
